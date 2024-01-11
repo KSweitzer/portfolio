e1e879537699f3e689047a489e6875f906fc6c02
Kevin Sweitzer <kevint.sweitzer@gmail.com>
2024-01-11 12:15:39 -0800

    created nav bar

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/')
+    link = models.URLField(blank=True)
